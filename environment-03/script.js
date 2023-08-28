5f699744d020bbe5d58295185142964fce96b7f7
Malte <maltebjoerklund@gmail.com>
2023-08-29 00:17:29 +0200

    update

--- environment-03/script.js
+++ environment-03/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+import { events } from "./data.js";
+
+function start() {
+  showEvents();
+}
+
+function showEvents() {
+  events.sort((a, b) => {
+    if (a.date < b.date) return -1;
+    return 1;
+  });
+  const date = new Date();
+  const currentDate = `${date.getFullYear()}-0${
+    date.getMonth() + 1
+  }-${date.getDate()}`;
+  console.log(currentDate);
+  for (const event of events) {
+    if (event.date > currentDate) {
+      document.querySelector("#events-list").insertAdjacentHTML(
+        "beforeend",
+        /* html */ `
+        <li>${event.date} - ${event.title}: ${event.description}</li>
+      `
+      );
+    }
+  }
+}
